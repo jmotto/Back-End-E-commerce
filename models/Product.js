adafe19385845ca33289ff4b6848db67be9b3de0
Jamie Otto <jmotto31@gmail.com>
2022-08-29 18:18:13 -0700

    defined columns for Product and Category models

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,7 +11,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // Validates that the value is a decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Set a default value of 10.
+      // Validates that the value is numeric.
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id',
+      }
+    }
   },
+
   {
     sequelize,
     timestamps: false,
